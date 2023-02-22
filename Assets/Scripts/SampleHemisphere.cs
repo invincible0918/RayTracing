ace98512a3c5e8bdef8a3c364fcbee07612d050b
invincible0918 <invincible0918@126.com>
2023-02-22 15:16:17 +0800

    Update SampleHemisphere.cs

--- Assets/Scripts/SampleHemisphere.cs
+++ Assets/Scripts/SampleHemisphere.cs
@@ -4,21 +4,54 @@ using UnityEngine;
 
 public class SampleHemisphere : MonoBehaviour
 {
-    ComputeShader cs;
+    public ComputeShader cs;
+    public int count = 100;
+    public float radius = 10f;
+    public bool isCosineSample;
 
-    public Transform inTrans;
-    public Transform outTrans;
+    ComputeBuffer cb;
+    Vector3[] directions;
 
-    public int lineCount = 10000;
 
     // http://corysimon.github.io/articles/uniformdistn-on-sphere/
     // Start is called before the first frame update
     void Start()
     {
+        cb = new ComputeBuffer(count, sizeof(float) * 3);
+        directions = new Vector3[count];
+        cs.SetBuffer(0, "directions", cb);
+        cs.SetBool("isCosineSample", isCosineSample);
+        cs.SetFloat("_Seed", Random.value);
+
+        cb.GetData(directions);
     }
 
     private void Update()
     {
-        cs.Dispatch(0, Mathf.CeilToInt(lineCount / 8f), 1, 1);
+        cs.GetKernelThreadGroupSizes(0, out uint x, out uint _, out _);
+
+        int groupX = Mathf.CeilToInt((float)count / x);
+
+        cs.SetBool("isCosineSample", isCosineSample);
+        cs.SetFloat("_Seed", Random.value);
+        cs.Dispatch(0, groupX, 1, 1);
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(Vector3.zero, radius);
+        if (directions != null)
+        {
+            for (int i = 0; i < directions.Length; ++i)
+            {
+                Gizmos.DrawLine(Vector3.zero, directions[i] * radius);
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        cb?.Release();
     }
 }
